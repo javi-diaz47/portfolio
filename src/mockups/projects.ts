488a34c9180c46b524511f415c74cd55f458d9d3
javi-diaz47 <javiereduardo300@gmail.com>
2024-07-31 11:12:28 -0500

    ✨ [ADD] add mockups

---
+++ src/mockups/projects.ts
@@ -0,0 +1,53 @@
+import type { Project } from "../types";
+
+export const PROJECTS: Project[] = [
+  {
+    title: "Pomotimer",
+    date: "2022",
+    techStack: [
+      { type: 'nextjs' },
+      { type: 'react' },
+      { type: 'ts' },
+      { type: 'js' },
+      { type: 'tailwind' },
+    ],
+    desc: "Web application that helps Colombian freelancers find work and increase their personal branding"
+  },
+  {
+    title: "Symulation",
+    date: "2022",
+    techStack: [
+      { type: 'nextjs' },
+      { type: 'react' },
+      { type: 'ts' },
+      { type: 'js' },
+      { type: 'tailwind' },
+    ],
+    desc: "Web application that helps Colombian freelancers find work and increase their personal branding"
+  },
+  {
+    title: "Web Entrepreneur Assistant",
+    date: "2022",
+    techStack: [
+      { type: 'nextjs' },
+      { type: 'react' },
+      { type: 'ts' },
+      { type: 'js' },
+      { type: 'tailwind' },
+    ],
+    desc: "Web application that helps Colombian freelancers find work and increase their personal branding"
+  },
+  {
+    title: "Pokedex",
+    date: "2022",
+    techStack: [
+      { type: 'nextjs' },
+      { type: 'react' },
+      { type: 'ts' },
+      { type: 'js' },
+      { type: 'tailwind' },
+    ],
+    desc: "Web application that helps Colombian freelancers find work and increase their personal branding"
+  },
+]
+
